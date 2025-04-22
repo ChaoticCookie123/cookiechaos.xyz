c455e6e92a04b4222b7377aac0328fcaafd57eeb
ZekISaMANNANM <123665551+ZekISaMANNANM@users.noreply.github.com>
2025-04-22 11:54:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
+let nb_stops = 10; // 10 color stops should be enough
+let dir = "left"; // left, right, top, bottom
+
+function SetupRainbow() {
+  var rainbowStr = GetRainbowString(nb_stops, 80, 50);
+  var oppositeDir = (dir==="left"?"right":(dir==="right"?"left":(dir==="top"?"bottom":"top")));
+  var css = '.rainbowText {\
+   background-clip: text;\
+   color: transparent;\
+   -webkit-background-clip: text;\
+   -webkit-text-fill-color: transparent;\
+   background-image: -webkit-linear-gradient(' + dir + ',' + rainbowStr + '); \
+   background-image: linear-gradient(to ' + oppositeDir + ',' + rainbowStr + ') \
+}'
+
+  var style = document.createElement("style");
+  style.type = 'text/css';
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+  document.head.appendChild(style);
+}
+
+
+// function that generate the rainbow string
+function GetRainbowString(nbStops, saturation, luminosity) {
+  let gap = 360 / nbStops,
+    colors = [];
+  for (let i = 0; i < nbStops; i++) {
+    colors.push("hsl(" + (i * gap) + "," + saturation + "%," + luminosity + "%)");
+  }
+  return colors.join();
+}
+
+
 
 window.addEventListener("load", function() {
-  var elements = document.getElementsByClassName("rainbowText");
-  for (let i = 0; i < elements.length; i++) {
-    generateRainbowText(elements[i]);
-  }
+  SetupRainbow();
 });
-
-function generateRainbowText(element) {
-  var text = element.innerText;
-  element.innerHTML = "";
-  for (let i = 0; i < text.length; i++) {
-    let charElem = document.createElement("span");
-    charElem.style.color = "hsl(" + (360 * i / text.length) + ",80%,50%)";
-    charElem.innerHTML = text[i];
-    element.appendChild(charElem);
-  }
-}
